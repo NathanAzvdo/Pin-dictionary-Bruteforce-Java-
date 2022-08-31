311d7c11f918f95e2724d0fc69e3e8ed9ff963cc
Nathan Azevedo <108633562+NathanAzvdo@users.noreply.github.com>
2022-08-31 10:01:18 -0300

    Add files via upload

---
+++ Brute/src/brute/Brute.java
@@ -0,0 +1,41 @@
+
+package brute;
+
+public class Brute {
+
+    public static void main(String[] args) {
+       String[] Num = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        for (int i = 0; i <10; i++) {
+         
+            for (int j = 0; j < 10; j++) {
+            
+                for (int k = 0; k < 10; k++) {
+                    
+                    for (int l = 0; l < 10; l++) {
+                        
+                        for (int m = 0; m < 10; m++) {
+                            
+                            for (int n = 0; n < 10; n++) {
+                                
+                                for (int o = 0; o < 10; o++) {
+                                    
+                                    for (int p = 0; p < 10; p++) {
+                                        
+                                        for (int q = 0; q < 10; q++) {
+                                            
+                                            for (int r = 0; r < 10; r++) {
+                                                
+        System.out.println(Num[i]+Num[j]+Num[k]+Num[l]+Num[m]+Num[n]+Num[o]+Num[p]+Num[q]+Num[r]);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+}
